9c2f26ca2fba296dc28f86ebda58e97041e91805
tian <1919174745@qq.com>
2023-03-09 18:17:45 +0800

    添加了animate.css

--- src/components/MyList.vue
+++ src/components/MyList.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="listBox">
         <ul class="uLis">
-            <my-item v-for="todoObj in todos" :key="todoObj.id" :todo="todoObj" />
+            <transition-group name="animate__animated animate__bounce" enter-active-class="animate__lightSpeedInRight"
+                leave-active-class="animate__lightSpeedOutLeft">
+                <my-item v-for="todoObj in todos" :key="todoObj.id" :todo="todoObj" />
+            </transition-group>
         </ul>
     </div>
 </template>
 <script>
 import MyItem from './MyItem.vue'
+import 'animate.css';
 export default {
     name: 'MyList',
     props: ['todos'],
@@ -22,6 +26,24 @@ export default {
     .uLis {
         width: 100%;
         list-style: none;
+    }
+}
+
+.todo-enter-active {
+    animation: abc 0.5s linear;
+}
+
+.todo-leave-active {
+    animation: abc 0.5s linear reverse;
+}
+
+@keyframes abc {
+    from {
+        transform: translate(-100%);
+    }
+
+    to {
+        transform: translate(0);
     }
 }
 </style>
